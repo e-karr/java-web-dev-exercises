eb164a003c4c6c8bc113ea682d51ab3df60ba0bb
Elizabeth Karr <e.karr93@icloud.com>
2023-05-23 09:56:08 -0500

    create and complete Area class. create Circle class

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,15 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+import java.lang.Math;
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        double radius = input.nextDouble();
+        input.close();
+
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+    }
+}
