439d21dac7dbf5b2d57ad0e72910a1ab037fa5eb
Elizabeth Karr <e.karr93@icloud.com>
2023-05-23 10:01:23 -0500

    replace area calculation with call to Circle class

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,7 +1,6 @@
 package org.launchcode.java.studios.areaofacircle;
 
 import java.util.Scanner;
-import java.lang.Math;
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -9,7 +8,7 @@ public class Area {
         double radius = input.nextDouble();
         input.close();
 
-        double area = Math.PI * radius * radius;
+        double area = Circle.getArea(radius);
         System.out.println("The area of a circle of radius " + radius + " is: " + area);
     }
 }
