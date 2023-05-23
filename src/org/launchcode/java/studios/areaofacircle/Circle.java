590513121d77b8e6e8bab934be8b18272d731a1d
Elizabeth Karr <e.karr93@icloud.com>
2023-05-23 09:58:39 -0500

    complete Circle class

--- src/org/launchcode/java/studios/areaofacircle/Circle.java
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -1,4 +1,9 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.lang.Math;
 public class Circle {
+
+    public static Double getArea(Double radius) {
+        return Math.PI * radius * radius;
+    }
 }
