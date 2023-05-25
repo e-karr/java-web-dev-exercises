6cd073df41dedec1fa465beef1511a6b3cf695ef
Elizabeth Karr <e.karr93@icloud.com>
2023-05-24 20:07:26 -0500

    add validation

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,12 +1,35 @@
 package org.launchcode.java.studios.areaofacircle;
 
 import java.util.Scanner;
+
+import static java.lang.Character.isDigit;
+
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
+
+        String userInput = input.nextLine();
         input.close();
+
+        if (userInput.isEmpty()) {
+            System.out.println("Error: must enter a value");
+            return;
+        }
+
+        for (int i = 0; i < userInput.length(); i++) {
+            if (!isDigit(userInput.charAt(i))) {
+                System.out.println("Error: must enter a number");
+                return;
+            }
+        }
+
+        double radius = Double.parseDouble(userInput);
+
+        if (radius < 1) {
+            System.out.println("Error: radius cannot be negative");
+            return;
+        }
 
         double area = Circle.getArea(radius);
         System.out.println("The area of a circle of radius " + radius + " is: " + area);
