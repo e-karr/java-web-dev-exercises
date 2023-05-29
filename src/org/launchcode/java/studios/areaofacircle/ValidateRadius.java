50fcac332adf0d252ca486a086cf16abcc4a4b43
Elizabeth Karr <e.karr93@icloud.com>
2023-05-29 16:59:16 -0500

    add validation for negative numbers

--- src/org/launchcode/java/studios/areaofacircle/ValidateRadius.java
+++ src/org/launchcode/java/studios/areaofacircle/ValidateRadius.java
@@ -16,6 +16,12 @@ public class ValidateRadius {
 
         if (!isDouble)
             return false;
+        else {
+            doubleRadius = Double.parseDouble(radius);
+
+            if (doubleRadius <= 0)
+                return false;
+        }
 
         return true;
     }
