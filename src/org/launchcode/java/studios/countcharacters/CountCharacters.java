a9c53cad07a17a16a8fc84ea319e3c45878ae000
Elizabeth Karr <e.karr93@icloud.com>
2023-05-29 16:12:09 -0500

    turn phrase into character array

--- src/org/launchcode/java/studios/countcharacters/CountCharacters.java
+++ src/org/launchcode/java/studios/countcharacters/CountCharacters.java
@@ -1,5 +1,7 @@
 package org.launchcode.java.studios.countcharacters;
 
+import java.util.Arrays;
+
 public class CountCharacters {
     public static void main(String[] args) {
         String phrase = "If the product of two terms is zero then common sense says " +
@@ -7,6 +9,8 @@ public class CountCharacters {
                 "move all the terms over to one side, you can put the quadratics into a form " +
                 "that can be factored allowing that side of the equation to equal zero. Once " +
                 "you’ve done that, it’s pretty straightforward from there.";
+
+        char[] phraseCharacterArray = phrase.toCharArray();
 
 
     }
