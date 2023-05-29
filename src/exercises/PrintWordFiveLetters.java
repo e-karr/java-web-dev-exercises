50b9c7fa8931bb2b3c47fe3393f23784a00f6ba3
Elizabeth Karr <e.karr93@icloud.com>
2023-05-29 14:30:42 -0500

    ArrayList Exercise part 3

--- src/exercises/PrintWordFiveLetters.java
+++ src/exercises/PrintWordFiveLetters.java
@@ -1,12 +1,17 @@
 package exercises;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PrintWordFiveLetters {
 
     public static void printWordFiveLetters(ArrayList<String> words) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Search for words with length: ");
+        int length = input.nextInt();
+
         for (String word : words) {
-            if (word.length() == 5) {
+            if (word.length() == length) {
                 System.out.println(word);
             }
         }
