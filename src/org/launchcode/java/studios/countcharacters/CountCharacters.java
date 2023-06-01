422fac945a626dacf5c203c334773552ebf54d74
Elizabeth Karr <e.karr93@icloud.com>
2023-05-31 19:29:56 -0500

    prompt user to enter string in command line

--- src/org/launchcode/java/studios/countcharacters/CountCharacters.java
+++ src/org/launchcode/java/studios/countcharacters/CountCharacters.java
@@ -2,16 +2,15 @@ package org.launchcode.java.studios.countcharacters;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import static java.lang.Character.isAlphabetic;
 
 public class CountCharacters {
     public static void main(String[] args) {
-        String phrase = "If the product of two terms is zero then common sense says " +
-                "at least one of the two terms has to be zero to start with. So if you " +
-                "move all the terms over to one side, you can put the quadratics into a form " +
-                "that can be factored allowing that side of the equation to equal zero. Once " +
-                "you’ve done that, it’s pretty straightforward from there.";
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter phrase: ");
+        String phrase = input.nextLine();
 
         phrase = phrase.toLowerCase();
 
