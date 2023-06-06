70d8cf49c30efe0db3903cdeb93f486ce3e0dc7f
Elizabeth Karr <e.karr93@icloud.com>
2023-06-06 09:43:51 -0500

    add getters and setters

--- src/org/launchcode/java/demos/lsn3classes1/Teacher.java
+++ src/org/launchcode/java/demos/lsn3classes1/Teacher.java
@@ -5,4 +5,30 @@ public class Teacher {
     private String lastName;
     private String subject;
     private int yearsTeaching;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public int getYearsTeaching() {
+        return yearsTeaching;
+    }
+
+    protected void setYearsTeaching(int aYearsTeaching) {
+        yearsTeaching = aYearsTeaching;
+    }
+    public void setSubject(String aSubject) {
+        subject = aSubject;
+    }
+    public void setFirstName(String aFirstName) {
+        firstName = aFirstName;
+    }
+    public void setLastName(String aLastName) {
+        lastName = aLastName;
+    }
 }
