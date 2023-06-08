76fe6db75ba634192ec0ed22d0395f1bdbd681f5
Elizabeth Karr <e.karr93@icloud.com>
2023-06-08 09:34:07 -0500

    create package and classes for Studio 4

---
+++ src/org/launchcode/java/studios/restaurantmenuapp/MenuItem.java
@@ -0,0 +1,4 @@
+package org.launchcode.java.studios.restaurantmenuapp;
+
+public class MenuItem {
+}
---
+++ src/org/launchcode/java/studios/restaurantmenuapp/restaurantAppRunner.java
@@ -0,0 +1,7 @@
+package org.launchcode.java.studios.restaurantmenuapp;
+
+public class restaurantAppRunner {
+    public static void main(String[] args) {
+
+    }
+}
