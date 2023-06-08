2f07da6baa6a2a1ee322f1827a4d67715cb72534
Elizabeth Karr <e.karr93@icloud.com>
2023-06-08 09:47:11 -0500

    add getters and setters and constructor to Menu class

--- src/org/launchcode/java/studios/restaurantmenuapp/Menu.java
+++ src/org/launchcode/java/studios/restaurantmenuapp/Menu.java
@@ -5,4 +5,24 @@ import java.util.ArrayList;
 public class Menu {
     private String type;
     private ArrayList<MenuItem> menuItems;
+
+    public Menu(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setMenuItems(ArrayList<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
 }
