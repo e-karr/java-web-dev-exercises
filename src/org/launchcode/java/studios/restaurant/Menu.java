111435bb6bea4a1725fbdd75d1d2d00937fa82c0
Elizabeth Karr <e.karr93@icloud.com>
2023-06-15 09:33:14 -0500

    add methods for add and remove items, tell when menu was last updated, print out entire menu

--- src/org/launchcode/java/studios/restaurant/Menu.java
+++ src/org/launchcode/java/studios/restaurant/Menu.java
@@ -1,13 +1,55 @@
 package org.launchcode.java.studios.restaurant;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Menu {
+    public static final int DAYS_NEW = 30;
     private String type;
     private ArrayList<MenuItem> menuItems;
+    private LocalDate lastUpdated = LocalDate.now();
+
+    public Menu(String type, ArrayList<MenuItem> menuItems) {
+        this.type = type;
+        this.menuItems = menuItems;
+    }
 
     public Menu(String type) {
-        this.type = type;
+        this(type, new ArrayList<>());
+    }
+
+    public boolean add(MenuItem newItem) {
+        boolean addItem = this.menuItems.add(newItem);
+
+        if (!addItem) {
+            return false;
+        }
+        setLastUpdated(LocalDate.now());
+        return true;
+    }
+
+    public boolean remove(MenuItem item) {
+        boolean removeItem = this.menuItems.remove(item);
+
+        if (!removeItem) {
+            return false;
+        }
+        setLastUpdated(LocalDate.now());
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        String fullMenu = "";
+        fullMenu += "Type: " + getType();
+        fullMenu += "\nLast Updated: " + getLastUpdated();
+        fullMenu += "\n**********";
+
+        for (MenuItem item : menuItems) {
+            fullMenu += "\n" + item;
+        }
+
+        return fullMenu;
     }
 
     public String getType() {
@@ -15,6 +57,7 @@ public class Menu {
     }
 
     public void setType(String type) {
+        setLastUpdated(LocalDate.now());
         this.type = type;
     }
 
@@ -23,6 +66,15 @@ public class Menu {
     }
 
     public void setMenuItems(ArrayList<MenuItem> menuItems) {
+        setLastUpdated(LocalDate.now());
         this.menuItems = menuItems;
+    }
+
+    public LocalDate getLastUpdated() {
+        return lastUpdated;
+    }
+
+    private void setLastUpdated(LocalDate lastUpdated) {
+        this.lastUpdated = lastUpdated;
     }
 }
