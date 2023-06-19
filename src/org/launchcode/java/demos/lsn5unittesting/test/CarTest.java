4e51b30b11d018ac396f071bd76889551d28ea40
Elizabeth Karr <e.karr93@icloud.com>
2023-06-19 14:47:07 -0500

    complete lesson 5 exercises

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -3,7 +3,6 @@ package org.launchcode.java.demos.lsn5unittesting.test;
 import org.junit.Before;
 import org.junit.Test;
 import org.launchcode.java.demos.lsn5unittesting.main.Car;
-
 import static org.junit.Assert.*;
 
 public class CarTest {
@@ -28,7 +27,22 @@ public class CarTest {
         assertFalse(test_car.getGasTankLevel() == 0);
     }
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void testGasTankAfterExceedingTankRange() {
+        test_car.drive(501);
+        assertEquals(0, test_car.getGasTankLevel(), .001);
+    }
     //TODO: can't have more gas than tank size, expect an exception
+    @Test(expected = IllegalArgumentException.class)
+    public void testGasOverfillException() {
+        test_car.addGas(5);
+        fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+    }
 
 }
