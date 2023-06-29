cd766585a802776910f02f0f315ca38c95ef702e
Elizabeth Karr <e.karr93@icloud.com>
2023-06-28 19:30:30 -0500

    create Question class

---
+++ src/org/launchcode/java/studios/quiz/Question.java
@@ -0,0 +1,32 @@
+package org.launchcode.java.studios.quiz;
+// Design
+// answers
+    // correct answer
+// question
+// points
+public abstract class Question {
+
+    private int pointValue;
+
+    private final String questionText;
+
+    public Question(int pointValue, String questionText) {
+        this.pointValue = pointValue;
+        this.questionText = questionText;
+    }
+
+    public void displayQuestion() {
+        System.out.println(questionText);
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public void setPointValue(int pointValue) {
+        this.pointValue = pointValue;
+    }
+
+    public abstract void displayAnswers();
+    public abstract void getAnswers();
+}
